a242236b6422d8a64112c2ed935a05950b0e39e0
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-30 16:35:41 +0530

    new app

---
+++ FirstAPP/src/com/gunjan/Main.java
@@ -0,0 +1,10 @@
+package com.gunjan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char a = 'A';
+        System.out.println("hello world");
+        System.out.println(a);
+    }
+}
